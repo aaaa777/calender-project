50423203e7478ded9a5b520364dd8116ad5f888d
aaaa777(hiupc) <aaaa777@users.noreply.github.com>
2021-08-21 18:16:08 +0900

    [create] プロシージャ定義の作成
    アカウント作成プロシージャの関数定義のみ

---
+++ db/ini/initialize_procedure.sql
@@ -0,0 +1,32 @@
+
+create procedure create_account()
+language sql
+as $$
+  begin;
+    
+  commit;
+$$;
+
+create procedure create_organization()
+language sql
+as $$
+  begin;
+
+  commit;
+$$;
+
+create procedure create_group()
+language sql
+as $$
+  begin;
+
+  commit;
+$$;
+
+create procedure create_plan()
+language sql
+as $$
+  begin;
+
+  commit;
+$$;
