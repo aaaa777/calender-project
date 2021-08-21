66b38be3f1f423a7284c6884b98ba0707f6c1ca4
aaaa777(hiupc) <aaaa777@users.noreply.github.com>
2021-08-21 21:36:26 +0900

    [update] テーブル初期化SQL一応の完成
    細かい型などを変える必要あり

--- db/ini/initialize_table.sql
+++ db/ini/initialize_table.sql
@@ -1,6 +1,6 @@
 
-drop table if not exists HostAccount;
-create table if not exists HostAccount(
+drop table if exists AccountList;
+create table AccountList(
   id serial unique,
   account_id varchar(30) primary key,
   account_name varchar(30) not null,
@@ -9,23 +9,23 @@ create table if not exists HostAccount(
   creation_timestamp timestamp without time zone default CURRENT_TIMESTAMP
 );
 
-drop table if not exists AccessToken;
-create table if not exists AccessToken(
+drop table if exists AccessToken;
+create table AccessToken(
   account_id varchar(30) primary key,
   access_token char(64) not null,
   /*勝手仕様 セッション有効期限*/
   expires_for timestamp
 );
 
-drop table if not exists AccountPassword;
-create table if not exists AccountPassword(
+drop table if exists AccountPassword;
+create table AccountPassword(
   account_id varchar(30) primary key,
   salt varchar(64) not null,
   password_hash varchar(64) not null
 );
 
-drop table if not exists OrganizationList;
-create table if not exists OrganizationList(
+drop table if exists OrganizationList;
+create table OrganizationList(
   organization_id varchar(30) primary key not null,
   organization_name varchar(30) not null,
   organization_address varchar(150),
@@ -33,8 +33,8 @@ create table if not exists OrganizationList(
   organization_token char(30)
 );
 
-drop table if not exists OrganizationInvitation;
-create table if not exists OrganizationInvitation(
+drop table if exists OrganizationInvitation;
+create table OrganizationInvitation(
   invite_token char(20) primary key,
   organization_id varchar(30) not null,
   /*下のテーブルとの型指定揺れあり*/
@@ -42,29 +42,55 @@ create table if not exists OrganizationInvitation(
   account_id varchar(30) not null
 )
 
-drop table if not exists OrganizationMember;
-create table if not exists OrganizationMember(
+drop table if exists MemberList;
+create table MemberList(
   member_id varchar(30) primary key,
   account_id varchar(30) not null,
   organization_id char(32) not null,
-  group_id char(32) not null,
+  -- group_id char(32) not null,
   member_name varchar(30) not null,
   phone_number varchar(15),
-  organization_mail varchar(255),
+  mail varchar(255),
   accepting_flag bool not null
 );
 
-drop table if not exists GroupDetail;
-create table if not exists GroupDetail(
-  group_id bigint
+drop table if exists GroupList;
+create table GroupList(
+  group_id bigint unique,
+  group_name varchar(30),
+  organization_id varchar(30),
+  group_author_member_id bigint,
+  group_phone_number bigint,
+  gtoup_flag boolean-- ,
+  -- group_token char(30)
 );
 
-create table if not exists t(
-  id bigint
+drop table if exists PlanList;
+create table PlanList(
+  plan_id bigint primary key,
+  plan_organization bigint not null,
+  plan_title varchar(30) not null,
+  plan_description varchar(200),
+  plan_group_id bigint,
+  plan_author_member_id bigint,
+  plan_host_member_id bigint,
+  plan_creation_date timestamp,
+  plan_delete_permission boolean,
+  plan_category_id char(20),
+  plan_location_url varchar(150),
+  plan_location_name varchar(20),
+  plan_event_id char(32),
+  plan_event_token char(32)
 );
 
-create table if not exists t(
-  id bigint
+drop table if exists PlanKeywordList;
+create table PlanKeywordList(
+  plan_id bigint primary key,
+  keyword varhar(30) not null
 );
 
-drop function if exists nallowflake();
+drop table if exists PlanMemberList;
+create table PlanMemberList(
+  plan_id bigint primary key,
+  member_id bigint not null
+)
