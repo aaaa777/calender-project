5f6fbb8850aa0f52fad8fb64db26d635fa002d01
aaaa777(hiupc) <aaaa777@users.noreply.github.com>
2021-08-21 18:21:24 +0900

    [create] シーケンスの初期化
    narrowflake id で使うシーケンステーブルの初期化ファイル

---
+++ db/ini/initialize_sequence.sql
@@ -0,0 +1,29 @@
+create sequence narrowflake_seq(
+  maxvalue 1023,
+  minvalue 0,
+  cycle
+);
+
+create sequence narrowflake_user(
+  maxvalue 1023,
+  minvalue 0,
+  cycle
+);
+
+create sequence narrowflake_organization(
+  maxvalue 1023,
+  minvalue 0,
+  cycle
+);
+
+create sequence narrowflake_group(
+  maxvalue 1023,
+  minvalue 0,
+  cycle
+);
+
+create sequence narrowflake_plan(
+  maxvalue 1023,
+  minvalue 0,
+  cycle
+);
