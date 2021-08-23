c315c084e36931aed27e44433b4083b594ba5382
aaaa777(hiupc) <aaaa777@users.noreply.github.com>
2021-08-23 15:02:10 +0900

    [update] create sequenceの構文間違えてたので修正

--- db/ini/initialize_sequence.sql
+++ db/ini/initialize_sequence.sql
@@ -1,35 +1,35 @@
 
 -- id: 1111
-create sequence narrowflake_seq(
-  maxvalue 1023,
-  minvalue 0,
+create sequence narrowflake_seq
+  maxvalue 1023
+  minvalue 0
   cycle
-);
+;
 
 -- id: 0001
-create sequence narrowflake_user(
-  maxvalue 1023,
-  minvalue 0,
+create sequence narrowflake_user
+  maxvalue 1023
+  minvalue 0
   cycle
-);
+;
 
 -- id: 0002
-create sequence narrowflake_organization(
-  maxvalue 1023,
-  minvalue 0,
+create sequence narrowflake_organization
+  maxvalue 1023
+  minvalue 0
   cycle
-);
+;
 
 -- id: 0003
-create sequence narrowflake_group(
-  maxvalue 1023,
-  minvalue 0,
+create sequence narrowflake_group
+  maxvalue 1023
+  minvalue 0
   cycle
-);
+;
 
 -- id: 0004
-create sequence narrowflake_plan(
-  maxvalue 1023,
-  minvalue 0,
+create sequence narrowflake_plan
+  maxvalue 1023
+  minvalue 0
   cycle
-);
+;
